e5b8b6a3b9dbfc74ee40393f0a96ceec84f7b07f
Douglas Berkley <douglasmberkley@gmail.com>
2020-07-08 19:12:25 +0900

    finalized stylying on the home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     # TODO: have JS make a request to app and not scrape again
+    @time_zones = ActiveSupport::TimeZone.all.sort
     timezone_name = params[:timezone]&.split('0) ')&.last || cookies[:timezone] || 'London'
     @timezone = ActiveSupport::TimeZone.all.find { |tz| tz.name == timezone_name }
     episodes = ScrapeMotdService.new(@timezone).call
