c6fcf273279e73ad807f13bce4222ae4edfcafd9
Douglas Berkley <douglasmberkley@gmail.com>
2020-07-08 14:32:52 +0900

    Added the timezone into cookies

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,10 @@
 class PagesController < ApplicationController
   def home
-    timezone_name = params[:timezone]&.split('0) ')&.last || 'London'
+    timezone_name = params[:timezone]&.split('0) ')&.last || cookies[:timezone] || 'London'
     @timezone = ActiveSupport::TimeZone.all.find { |tz| tz.name == timezone_name }
     episodes = ScrapeMotdService.new(@timezone).call
     @future = episodes[:future]
     @past = episodes[:past]
+    cookies[:timezone] = timezone_name
   end
 end
