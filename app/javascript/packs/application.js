b42e502abe8b79fa1658928f0e024bd5f3c29efb
Douglas Berkley <douglasmberkley@gmail.com>
2020-07-08 13:48:58 +0900

    added JS for timezone change

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,6 +29,12 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
+  const timeZones = document.querySelector('#time_zones')
+  if (timeZones) {
+    timeZones.addEventListener('change', (event) => {
+      const newZone = event.currentTarget.value
+      let url = window.location.href.split('?')[0] += `?timezone=${newZone}`;
+      window.location.href = url;
+    })
+  }
 });
