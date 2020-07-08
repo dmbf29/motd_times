e9f74f6b66b3246f915e509efa96144ce6f438c3
Douglas Berkley <douglasmberkley@gmail.com>
2020-07-08 11:27:07 +0900

    set default timezone

--- app/services/scrape_motd_service.rb
+++ app/services/scrape_motd_service.rb
@@ -10,8 +10,7 @@ class ScrapeMotdService
   def call
     html_content = open('https://www.bbc.co.uk/programmes/m0007fnr/episodes/guide.2013inc?nestedlevel=3').read
     doc = Nokogiri::HTML(html_content)
-    past = []
-    future = []
+    episodes = { past: [], future: [] }
     doc.search('.episode-guide__episode').each do |element|
       date = Date.parse(element.search('.programme__titles').text.strip)
       if date > Date.today
@@ -19,13 +18,11 @@ class ScrapeMotdService
         Time.zone = 'London'
         time = Time.zone.parse(time)
         dt = Time.zone.local(date.year, date.month, date.day, time.hour, time.min, time.sec).in_time_zone
-        future << dt
+        episodes[:future] << dt
       else
-        past << date
+        episodes[:past] << date
       end
     end
-
+    episodes
   end
-
-
 end
