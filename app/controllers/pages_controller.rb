adf2373c426c8b3b9ffffa68f51c20ee9e5df7c2
Douglas Berkley <douglasmberkley@gmail.com>
2020-07-09 19:36:32 +0900

    updated view and controller for episodes in the DB

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,11 @@
-require 'reddit/base'
-
 class PagesController < ApplicationController
   def home
     # Scraping moved to a background job
     @time_zones = ActiveSupport::TimeZone.all.sort
     timezone_name = params[:timezone]&.split('0) ')&.last || cookies[:timezone] || 'London'
     @timezone = ActiveSupport::TimeZone.all.find { |tz| tz.name == timezone_name }
-    episodes = ScrapeMotdService.new(@timezone).call
-    @future = episodes[:future].sort_by { |ep| ep.time }
-    @past = episodes[:past].sort_by { |ep| ep.time }.reverse
+    @future = Episode.where(past: false).order(:date)
+    @past = Episode.where(past: true).order(date: :desc)
     cookies[:timezone] = timezone_name
   end
 end
